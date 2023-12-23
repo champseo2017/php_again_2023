66252b1cea33839b32ac1d35bd975333369f24ae
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2023-12-23 17:03:48 +0700

    ตรวจสอบค่าสุดท้ายของ อาร์เรย์ ใช้ฟังก์ชัน end()

--- class.product.php
+++ class.product.php
@@ -1,24 +1,21 @@
 <?php
 
-// ใช้ __get(), __set()
+class Product {
+    private $id;
+    private $name;
 
-class product {
-    private $data = array();
-
-    public function __set( $name, $value ) {
-        $this->data[ $name ] = $value;
-    }
-
-    public function __get( $name ) {
-        return $this->data[ $name ];
+    public function __construct($id, $name) {
+        $this->id = $id;
+        $this->name = $name;
     }
 
     public function info() {
-        $info = '';
-        foreach ( $this->data as $data ) {
-            $info .= $data . ' ';
+        $info = "";
+        $lastProperty = end($this); // ตรวจสอบค่าสุดท้ายของ อาร์เรย์ ใช้ฟังก์ชัน end()
+        foreach ($this as $key => $value) {
+           if ($value == $lastProperty) $info .= $value;
+           else $info .= $value . ":";
         }
-
         return $info;
     }
 }
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include 'class.product.php';
-$product = new Product();
-$product->id = 1;
-$product->name = 'TV';
-echo $product->info();
+$s1 = new Product(1, "TV");
+echo $s1->info();
+
+// http://localhost:8080/php_again_2023/
